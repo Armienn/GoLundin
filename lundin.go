d982a774b02927a8e1e43b50d47951ddfe43cfdc
Armienn <nigathr@gmail.com>
2016-12-20 13:29:03 +0100

    Now uses Armienn/GoServer

--- lundin.go
+++ lundin.go
@@ -4,115 +4,34 @@ import (
 	"html/template"
 	"io/ioutil"
 	"net/http"
-	"regexp"
-	"strings"
 
-	"github.com/Armienn/GoLundin/session"
-	_ "github.com/Armienn/GoLundin/session/provider/memory"
+	"github.com/Armienn/GoServer"
 )
 
 func main() {
-	http.HandleFunc("/view/", makeHandler(viewHandler))
-	http.HandleFunc("/edit/", makeHandler(editHandler))
-	http.HandleFunc("/save/", makeHandler(saveHandler))
-
-	http.ListenAndServe(":8080", nil)
+	server := goserver.NewServer(false)
+	server.AddHandler("/js/", jsHandler)
+	server.AddHandler("/", viewHandler)
+	server.Serve()
 }
 
-func login(w http.ResponseWriter, r *http.Request) {
-	sess := session.GlobalSessions.SessionStart(w, r)
-	r.ParseForm()
-	if r.Method == "GET" {
-		t, _ := template.ParseFiles("login.gtpl")
-		w.Header().Set("Content-Type", "text/html")
-		t.Execute(w, sess.Get("username"))
+func viewHandler(server *goserver.Server, w http.ResponseWriter, r *http.Request, path string, session goserver.Session, user interface{}) {
+	data := struct{ Count int }{0}
+	value, ok := session.Get("musle")
+	if ok {
+		data.Count = value.(int)
+	}
+	data.Count++
+	session.Set("musle", data.Count)
+	temp, err := template.ParseFiles("test.html")
+	if err != nil {
+		w.Write([]byte("Fejl: " + err.Error()))
 	} else {
-		sess.Set("username", r.Form["username"])
-		http.Redirect(w, r, "/", 302)
+		temp.Execute(w, data)
 	}
 }
 
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-		return
-	}
-	renderTemplate(w, "view", p)
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{Title: title}
-	}
-	renderTemplate(w, "edit", p)
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-	body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
-
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil {
-			http.NotFound(w, r)
-			return
-		}
-		fn(w, r, m[2])
-	}
-}
-
-func loadTemplates() {
-	var allFiles []string
-	files, err := ioutil.ReadDir("./templates")
-	if err != nil {
-		panic("Couldn't read the template folder")
-	}
-	for _, file := range files {
-		filename := file.Name()
-		if strings.HasSuffix(filename, ".tmpl") {
-			allFiles = append(allFiles, "./templates/"+filename)
-		}
-	}
-
-	templates, err = template.ParseFiles(allFiles...) //parses all .tmpl files in the 'templates' folder
+func jsHandler(server *goserver.Server, w http.ResponseWriter, r *http.Request, path string, session goserver.Session, user interface{}) {
+	file, _ := ioutil.ReadFile("gopher/" + path)
+	w.Write(file)
 }
