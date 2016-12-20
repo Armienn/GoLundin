0c57ba1b4b9afc95603d689f5a271300b5d958b9
Armienn <nigathr@gmail.com>
2016-12-20 13:55:02 +0100

    Added login

--- lundin.go
+++ lundin.go
@@ -5,13 +5,19 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	"encoding/json"
+
 	"github.com/Armienn/GoServer"
 )
 
 func main() {
-	server := goserver.NewServer(false)
+	server := goserver.NewServer(true)
 	server.AddHandler("/js/", jsHandler)
 	server.AddHandler("/", viewHandler)
+	users := loadUsers()
+	for _, user := range users {
+		server.AddUser(user.Name, user.Password)
+	}
 	server.Serve()
 }
 
@@ -34,4 +40,30 @@ func viewHandler(server *goserver.Server, w http.ResponseWriter, r *http.Request
 func jsHandler(server *goserver.Server, w http.ResponseWriter, r *http.Request, path string, session goserver.Session, user interface{}) {
 	file, _ := ioutil.ReadFile("gopher/" + path)
 	w.Write(file)
+}
+
+type User struct {
+	Name     string
+	Password string
+}
+
+func loadUsers() []User {
+	file, err := ioutil.ReadFile("users.json")
+	if err != nil {
+		panic(err)
+	}
+	var users []User
+	err = json.Unmarshal(file, &users)
+	if err != nil {
+		panic(err)
+	}
+	return users
+}
+
+func toJson(thing interface{}) string {
+	bytes, err := json.Marshal(thing)
+	if err != nil {
+		return "{\"error\":\"error\"}"
+	}
+	return string(bytes)
 }
