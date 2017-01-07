d8c490a4322abdc21135c37a0a064c2ad9171ff1
Armienn <nigathr@gmail.com>
2017-01-07 23:03:10 +0100

    Updated test

--- files/js/test.js
+++ files/js/test.js
@@ -2,15 +2,17 @@ var body = document.getElementById('jsbox')
 body.innerHTML = ""
 var canvas = document.createElement("canvas")
 body.appendChild(canvas)
+canvas.width = 800
+canvas.height = 660
 var ctx = canvas.getContext('2d')
 
 function clear() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 }
 
-function mulle() {
+function draw() {
     clear()
-    var second = new Date().getSeconds()
+    var second = new Date().getMilliseconds()/5
     ctx.fillStyle = "rgba(0,100,100,0.5)"
     ctx.fillRect (second, 20, 50, 50)
 }
@@ -18,4 +20,4 @@ function mulle() {
 if (updateInterval) {
     clearInterval(updateInterval)
 }
-var updateInterval = setInterval(mulle, 1000)
+var updateInterval = setInterval(draw, 50)
